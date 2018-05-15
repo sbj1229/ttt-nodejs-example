cbe41c9bf4779bf74ed7223dc498322c93adc1be
sbj1229 <37063431+sbj1229@users.noreply.github.com>
2018-05-15 14:51:54 +0900

    변경

--- index.js
+++ index.js
@@ -7,7 +7,26 @@ app.use(cors()); // http://guswnsxodlf.github.io/enable-CORS-on-express
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-var gameState = { isXnext: true, squares: Array(9).fill('') };
+var gameState = {winner:'', isXnext: true, squares: Array(9).fill('') };
+
+function wins(turn){
+  if((gameState.squares[0]==turn && gameState.squares[1]==turn && gameState.squares[2]==turn) ||
+  (gameState.squares[3]==turn && gameState.squares[4]==turn && gameState.squares[5]==turn) ||
+  (gameState.squares[6]==turn && gameState.squares[7]==turn && gameState.squares[8]==turn) ||
+  (gameState.squares[0]==turn && gameState.squares[3]==turn && gameState.squares[6]==turn) ||
+  (gameState.squares[1]==turn && gameState.squares[4]==turn && gameState.squares[7]==turn) ||
+  (gameState.squares[2]==turn && gameState.squares[5]==turn && gameState.squares[8]==turn) ||
+  (gameState.squares[0]==turn && gameState.squares[4]==turn && gameState.squares[8]==turn) ||
+  (gameState.squares[2]==turn && gameState.squares[4]==turn && gameState.squares[6]==turn))
+  return true;
+  else  return false;
+};
+
+function Winner(){
+  if(wins('X')) return'X';
+  if(wins('O')) return 'O';
+  return '';
+};
 
 app.get('/game_state', (req, res) => {
   res.charset = 'UTF-8';
@@ -19,10 +38,28 @@ app.get('/move', (req, res) => {
   res.charset = 'UTF-8';
   var turn = req.query.turn;
   var pos = req.query.pos;
-  console.log('move: '+turn+pos);
-  gameState.squares[pos] = turn;
-  gameState.isXnext = !gameState.isXnext;
-  res.send('OK');
+  if((gameState.isXnext && turn=="O") || (!gameState.isXnext && turn=="X"))
+  {
+    res.send('턴이 아님');
+  }
+  else{
+    if(Winner() == 'X' || Winner() == 'O' )
+    {
+      res.send('게임이 종료됨');
+      return;
+    }
+    console.log('move: '+turn+pos);
+    if(gameState.squares[pos]!='')
+    {
+      res.send('빈칸이 아님');
+    }
+    else{
+      gameState.squares[pos] = turn;
+      gameState.isXnext = !gameState.isXnext;
+      gameState.winner = Winner();
+    }
+
+  }
 } );
 
 app.get('/', (req, res) => {
